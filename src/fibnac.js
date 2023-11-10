ca965489c2fc1d402801582da74201fc3dcc4333
Infidea <bobby@infidea.cn>
2023-11-10 15:49:00 +0800

    新增运行结果；

--- src/fibnac.js
+++ src/fibnac.js
@@ -1,38 +1,44 @@
-function fibonacci(n){
-    return n < 1 ? 0
-         : n<= 2 ? 1
-         : fibonacci(n-1) + fibonacci(n-2);
+/**
+ * 本次参考资料
+ * 链接：https://www.bilibili.com/video/BV1JN41147ob
+ */
+
+function fibonacci(n) {
+  return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-const doFib = (iterations) => {
-    new Promise((resolve, reject) => {
-        const start = Date.now();
-        const res = fibonacci(iterations);
-        console.log(`doFib done in: ${Date.now() - start}ms`);
-        resolve(res);
-    })
-}
-
-const main = async () =>{
+// 返回一个promise
+const doFib = (iterations) =>
+  new Promise((resolve, reject) => {
     const start = Date.now();
+    const res = fibonacci(iterations);
+    console.log(`doFib done in: ${Date.now() - start}ms`);
+    resolve(res);
+  });
 
-    const values = await Promise.all([
-        doFib(40),
-        doFib(40),
-        doFib(40),
-        doFib(40),
-        doFib(40),
-        doFib(40),
-        doFib(40),
-        doFib(40),
-        doFib(40),
-        doFib(40)
-    ])
+const main = async () => {
+  const start = Date.now();
 
-    console.log('values:', values);
-    console.log(`fib done in ${Date.now() - start}ms`);
-}
+  // 没有预期执行并发执行所有调度，它实际上是一个接一个的
+  const values = await Promise.all([
+    doFib(40),
+    doFib(40),
+    doFib(40),
+    doFib(40),
+    doFib(40),
+    doFib(40),
+    doFib(40),
+    doFib(40),
+    doFib(40),
+    doFib(40),
+  ]);
 
-main().catch((e)=>{
-    console.error(e);
-})
+  console.log("values:", values);
+  console.log(`fib done in ${Date.now() - start}ms`);
+};
+
+main().catch((e) => {
+  console.error(e);
+});
+
+
